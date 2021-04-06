4e5b34e865db4b942326eda1175860d85a8572f6
Roy A Vanegas <roy@thecodeeducators.com>
2021-04-06 15:36:32 -0400

    Add example of getElementsByClassName

---
+++ javascript/establishing-references-to-dom-nodes/get-elements-by-class-name/js/app.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    let highlights = document.getElementsByClassName(`highlight`);
+
+    highlights[0].style.backgroundColor = `yellow`;
+    highlights[1].style.backgroundColor = `pink`;
+    highlights[2].style.backgroundColor = `lightblue`;
+};
