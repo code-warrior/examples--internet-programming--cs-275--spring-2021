96230abf708b79b0432f0d4b63dc774562a40b41
Roy A Vanegas <roy@thecodeeducators.com>
2021-04-06 14:39:46 -0400

    Add example of getElementById

---
+++ javascript/establishing-references-to-dom-nodes/get-element-by-id/js/app.js
@@ -0,0 +1,5 @@
+window.onload = () => {
+    let firstParagraph = document.getElementById(`first-paragraph`);
+    firstParagraph.textContent = `This <em>is</em> the first paragraph`;
+    // firstParagraph.innerHTML = `This <em>is</em> the first paragraph`;
+};
